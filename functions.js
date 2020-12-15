aea6e95378f1786ea9b04bb329836936ad9e2614
fidelhen <fidelhen@gmail.com>
2020-12-15 16:12:36 -0500

    Added new AJAX request for contact form

--- functions.js
+++ functions.js
@@ -18,4 +18,51 @@ $(document).ready(function() {
     //How it works title slide up fade
     $('#slide-up-animation').hide();
     $('#slide-up-animation').slideToggle("slow");
+
+    //Page would refresh everytime you hit submit so I took it off
+    $("#form-fields").submit(function(form) {
+        form.preventDefault();
+    });
 });
+
+//Functions
+function contact(form) {
+    const firstName = this.$('#firstName').val();
+    const lastName = this.$('#lastName').val();
+    const email = this.$('#emailAddress').val();
+    const message = this.$('#messageField').val();
+
+    $("#submit-btn").html("Sending...");
+
+    uploadData(`${firstName} ${lastName}`, email, message);
+}
+
+function uploadData(name, email, message) {
+    var param = {name: name, email: email, message: message};
+    
+    //Ajax post to firebase
+    $.post('https://gimmi-website-default-rtdb.firebaseio.com/inquiries.json',JSON.stringify(param),function () {
+
+        //Ajax get to a joke api to lighten up as a thanks 
+        $.get('https://sv443.net/jokeapi/v2/joke/Programming,Pun?type=twopart?blacklistFlags=nsfw,racist,sexist', function(data){
+            var setup = data["setup"];
+            var delivery = data["delivery"];
+            
+            $('#joke-setup').text(`${setup}`);
+            $('#joke-delivery').text(`${delivery}`);
+            $('#myModal').modal('show'); 
+
+            resetForm();
+        });
+    });
+}
+
+function resetForm() {
+    $(':input','#form-fields')
+    .not(':button, :submit, :reset, :hidden')
+    .val('')
+    .prop('checked', false)
+    .prop('selected', false);
+
+    $("#submit-btn").html("Submit");
+}
